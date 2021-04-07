4637f2bfad70977c1460fa233f914450798d1e9d
vladimirangarita <vladimir.angarita@gmail.com>
2021-04-07 19:58:43 -0400

    Agregar archivos de proyecto.

---
+++ MiPrimeraWebAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MiPrimeraWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ MiPrimeraWebAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MiPrimeraWebAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
