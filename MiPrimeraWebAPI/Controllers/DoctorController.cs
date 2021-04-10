3a558fec0521af24c453fefafd8a25a6edfb5bc9
vladimirangarita <vladimir.angarita@gmail.com>
2021-04-10 13:33:45 -0400

    Capitulo 20 listo primer metodo del WebAPI

--- MiPrimeraWebAPI/Controllers/DoctorController.cs
+++ MiPrimeraWebAPI/Controllers/DoctorController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MiPrimeraWebAPI.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -9,5 +10,36 @@ namespace MiPrimeraWebAPI.Controllers
 {
     public class DoctorController : ApiController
     {
+        [HttpGet]
+        public IEnumerable<DoctorCLS>ListarDoctor()
+        {
+
+            using (BDDoctorDataContext bd = new BDDoctorDataContext())
+            {
+                IEnumerable<DoctorCLS> ListaDoctor = (from doctor in bd.Doctor
+                                                      join clinica in bd.Clinica
+                                                      on doctor.IIDCLINICA equals
+                                                      clinica.IIDCLINICA
+                                                      join especialidad in bd.Especialidad
+                                                      on doctor.IIDESPECIALIDAD equals
+                                                      especialidad.IIDESPECIALIDAD
+                                                      where doctor.BHABILITADO == 1
+                                                      select new DoctorCLS
+
+                                                      {
+                                                          iiDoctor = doctor.IIDDOCTOR,
+                                                          nombreClinica = clinica.NOMBRE,
+                                                          nombreEspecialidad = especialidad.NOMBRE,
+                                                          email = doctor.EMAIL,
+                                                          fechaContrato = (DateTime)doctor.FECHACONTRATO,
+                                                          nombreCompleto = doctor.NOMBRE + " " + doctor.APPATERNO
+                                                          + " " + doctor.APMATERNO
+                                                      }).ToList();
+
+
+                return ListaDoctor;
+            }
+            }
+
     }
 }
--- MiPrimeraWebAPI/Models/DoctorCLS.cs
+++ MiPrimeraWebAPI/Models/DoctorCLS.cs
@@ -7,5 +7,13 @@ namespace MiPrimeraWebAPI.Models
 {
     public class DoctorCLS
     {
+        public int iiDoctor { get; set; }
+        public string nombreCompleto { get; set; }
+        public string nombreClinica { get; set; }
+
+        public string nombreEspecialidad { get; set; }
+        public string email { get; set; }
+
+        public DateTime fechaContrato { get; set; }
     }
 }
